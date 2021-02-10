e513ced106139541369532017aa52a1ed52c60fe
李满满 <limanman@corp.netease.com>
2021-02-10 16:20:58 +0800

    :underage:

--- setup.py
+++ setup.py
@@ -11,11 +11,11 @@ from setuptools import find_packages, setup
 
 
 curdir = os.path.abspath(os.path.dirname(__file__))
-readme = open(os.path.join(curdir, 'README.md'), 'rb', 'utf-8').read()
+readme = open(os.path.join(curdir, 'READIT.md'), 'rb', 'utf-8').read()
 
 setup(
     name='namekox-websocket',
-    version='0.0.10',
+    version='0.0.11',
     description='namekox websocket',
     long_description_content_type='text/markdown',
     long_description=readme,
